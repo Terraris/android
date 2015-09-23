a1b479b2f1ba47dd33a37e418acdc5bcc92d3b7d
René Schönfelder <schoenfr@isp.uni-luebeck.de>
2015-09-23 13:16:58 +0200

    migrated current state from other repository and switched to gradle

---
+++ app/src/main/java/de/uniluebeck/greennav/AppContext.java
@@ -0,0 +1,11 @@
+package de.uniluebeck.greennav;
+
+import android.app.Application;
+
+/**
+ * This class provides the Application context
+ */
+public class AppContext extends Application
+{
+    public Tracker tracker = null;
+}
