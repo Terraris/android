72edd738415a68a1a2eac4247b6e6d1eebbcc458
Niko <Spam-Konto-Terraris@gmx.de>
2015-10-02 14:51:14 +0200

    added Style (Material Design) added Settings prepared connector for frontend cleaned project

--- app/src/main/java/de/uniluebeck/greennav/MainActivity.java
+++ app/src/main/java/de/uniluebeck/greennav/MainActivity.java
@@ -1,10 +1,19 @@
 package de.uniluebeck.greennav;
 
-import android.app.Activity;
+
 import android.content.Intent;
-import android.os.Build;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -23,18 +32,29 @@ import android.widget.TextView;
  * This is the MainActivity of the Application. It provides a Webview for input purposes, some
  * debug features and an instance of a JavaScriptInterface (JSInterface)
  */
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity implements SensorEventListener
 {
     /* permissionString to call for permission on runtime */
     private String[] permissions = {
             "android.permission.ACCESS_FINE_LOCATION"
     };
+    private Toolbar toolbar;
     private int RequestId = 1;
     private TextView origin;
+
+    private String serverURL = "http://192.168.2.100:8112/index.html";
+    private String serverIP = "192.168.2.100:8112";
+    private String stringDegree;
+    private String stringHeading;
+
+    private boolean eval;
+    private boolean detail;
+    private float degree;
+    private SensorManager sensorManager;
+    private Tracker tracker;
     WebView myMainWebView;
     JSInterface jsInterface;
-    private String serverURL = "http://192.168.2.100:8112/index.html";
-
+    AppContext app;
 
     /**
      * onCreate is called when the application starts
@@ -48,13 +68,24 @@ public class MainActivity extends Activity
         //if (Build.VERSION.SDK_INT > 22)
         //    requestPermissions(permissions, RequestId);
 
+
         Bundle extras = getIntent().getExtras();
-        if(extras != null)
+        if (extras != null)
         {
-           serverURL = extras.getString("SERVER_URL");
+            serverURL = extras.getString("SERVER_URL");
+            eval = extras.getBoolean("EVALUATION_MODE");
+            detail = extras.getBoolean("DETAIL_MODE");
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+        /* set context */
+        app = (AppContext) getApplicationContext();
+
+        toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
 
         /* set a new Context */
         final AppContext app = (AppContext) getApplicationContext();
@@ -139,14 +170,143 @@ public class MainActivity extends Activity
             @Override
             public void onClick(View view)
             {
-                /* start Navigator */
-                Intent intent;
-                intent = new Intent(MainActivity.this, Navigator.class);
-                intent.putExtra("SERVER_URL", serverURL);
-                startActivity(intent);
-                finish();
+                Log.d("GreenNav Android", "Course: " + stringDegree + "°" + ", heading: " + stringHeading);
+                Log.d("GreenNav Android", "Evaluationmode active? " + eval);
+                Log.d("GreenNav Android", "Detailmode active?" + detail);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
+
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_settings));
+        // searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        //searchView.setOnQueryTextListener();
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        /* sensor registered listeners */
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        /* compass: save battery if paused */
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        getOrientation(sensorEvent);
+        stringDegree = String.format("%.1f", degree);
+        stringHeading = calculateDirection(degree);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle presses on the action bar items
+        switch (item.getItemId())
+        {
+            case R.id.action_settings:
+                openSettings();
+                return true;
+            //case R.id.action_search:
+            //openSearch();
+            //return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i)
+    {
+    }
+
+    /**
+     * compass
+     * sets the actual degree of the compass
+     */
+    private void getOrientation(SensorEvent sensorEvent)
+    {
+        Log.d("GreenNav Android", "getOrientation(), value: " + sensorEvent.values[0]);
+        degree = sensorEvent.values[0];
+    }
+
+
+    /**
+     * calcuates the cardinal direction
+     *
+     * @param degree - the degree of x-axis
+     * @return heading - the heading as string
+     */
+    protected String calculateDirection(float degree)
+    {
+        String heading = "";
+
+        if (degree >= 337.5 || degree < 22.5)
+        {
+            heading = "N";
+        }
+        if (degree >= 22.5 && degree < 67.5)
+        {
+            heading = "NE";
+        }
+        if (degree >= 67.5 && degree < 112.5)
+        {
+            heading = "E";
+        }
+        if (degree >= 112.5 && degree < 157.5)
+        {
+            heading = "SE";
+        }
+        if (degree >= 157.5 && degree < 202.5)
+        {
+            heading = "S";
+        }
+        if (degree >= 202.5 && degree < 247.5)
+        {
+            heading = "SW";
+        }
+        if (degree >= 247.5 && degree < 292.5)
+        {
+            heading = "W";
+        }
+        if (degree >= 292.5 && degree < 337.5)
+        {
+            heading = "NW";
+        }
+        return heading;
+    }
+
+    /**
+     * finds the devices' loaction and sets origins' text
+     */
+    private void findLocation()
+    {
+        this.tracker = app.tracker;
+        double[] myCoordinates = app.tracker.track();
+        Log.d("FindLocation", "Your location: " + myCoordinates[0] + "° N , " + myCoordinates[1] + "° E");
+    }
+
+    private void openSettings()
+    {
+        Intent intent;
+        intent = new Intent(MainActivity.this, Settings.class);
+        intent.putExtra("SERVER_IP", serverIP);
+        startActivity(intent);
+        finish();
     }
 
     /**
---
+++ app/src/main/java/de/uniluebeck/greennav/Settings.java
@@ -0,0 +1,105 @@
+package de.uniluebeck.greennav;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Switch;
+
+/**
+ * This class provides the welcome screen, which is used to set the serverURLs
+ */
+public class Settings extends Activity
+{
+
+    private String server;
+    private String serverURL;
+    private Switch evaluationSwitch;
+    private Switch detailSwitch;
+    private Button start;
+    private Boolean eval = false;
+    private Boolean detail = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_layout);
+
+        fade();
+        final EditText serverText = (EditText) findViewById(R.id.serverEditText);
+        Bundle extras = getIntent().getExtras();
+        serverText.setText(extras.getString("SERVER_IP"));
+
+        start = (Button) findViewById(R.id.startApp);
+        start.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                server = serverText.getText().toString();
+                serverURL = "http://" + server + "/index.html";
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("SERVER_URL", serverURL);
+                if (eval)
+                {
+                    intent.putExtra("EVALUATION_MODE", true);
+                }
+                if (detail)
+                {
+                    intent.putExtra("DETAIL_MODE", true);
+                }
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        evaluationSwitch = (Switch) findViewById(R.id.eval_switch);
+        evaluationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    eval = true;
+                } else
+                {
+                    eval = false;
+                }
+            }
+        });
+
+        detailSwitch = (Switch) findViewById(R.id.detail_switch);
+        detailSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if (isChecked)
+                {
+                    detail = true;
+                } else
+                {
+                    detail = false;
+                }
+            }
+        });
+
+
+    }
+
+    public void fade()
+    {
+        ImageView image = (ImageView) findViewById(R.id.imageView);
+        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+        image.startAnimation(animation1);
+
+    }
+}
